11f3c05a459827b19858fdca0c46c47732cfe723
lynaannou <lynaannou@gmail.com>
2025-11-25 10:35:24 +0100

    commit yet again

--- dz/usthb/eclipseworkspace/workspace/dao/DaoWorkspace.java
+++ dz/usthb/eclipseworkspace/workspace/dao/DaoWorkspace.java
@@ -2,7 +2,33 @@ public class DaoWorkspace extends Dao<Workspace> {
     private List<Workspace> workspaces = new ArrayList<>();
 
     public DaoWorkspace() {
-        workspaces.add(new Workspace())
+        workspaces.add(new Workspace("project test", 4, "15/11/2025", 0, 0, 0));
+    }
+    @Override
+    public Optional<Workspace> get (int team_id) {
+        return Optional.ofNullables(workspaces.get(team_id));
+
+    }
+    @Override
+    public List<Workspace> getAll() {
+        return workspaces;
+
+    }
+    @Override
+    public void save(Workspace workspace) {
+        workspaces.add(workspace);
+
+    }
+    @Override
+    public void update(Workspace workspace, String[] params) {
+        workspaces.setName(Objects.requireNotNull(
+            params[0], "Name cannot be null"
+        ));
+        workspaces.add(workspace);
+    }
+    @Override
+    public void delete(Workspace workspace) {
+        workspaces.remove(workspace);
     }
     
 }
