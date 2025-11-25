a9db85b646e337557b64168bc39038c1dad57ca4
lynaannou <lynaannou@gmail.com>
2025-11-25 08:29:42 +0100

    adding stuff we should organize this a bit later

---
+++ dz/usthb/eclipseworkspace/Dao.java
@@ -0,0 +1,13 @@
+public public interface Dao<T> {
+    
+    Optional<T> get(long id);
+    
+    List<T> getAll();
+    
+    void save(T t);
+    
+    void update(T t, String[] params);
+    
+    void delete(T t);
+} 
+
